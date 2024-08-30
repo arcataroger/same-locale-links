737a59970e1b1f02ba5bf3a389585754bf927466
Roger Tuan <r.tuan@datocms.com>
2024-08-30 00:10:22 -0700

    Basic functionality

--- src/components/SameLocaleLinks.tsx
+++ src/components/SameLocaleLinks.tsx
@@ -1,5 +1,5 @@
 import {Canvas, FieldGroup, Form, SelectField} from 'datocms-react-ui';
-import {RenderFieldExtensionCtx} from "datocms-plugin-sdk";
+import {Field, RenderFieldExtensionCtx} from "datocms-plugin-sdk";
 import {Client} from '@datocms/cma-client'
 import {useEffect, useMemo, useState} from "react";
 import type {Item} from '@datocms/cma-client/dist/types/generated/SimpleSchemaTypes'
@@ -22,16 +22,84 @@ const getRecordsByItemTypes = async ({itemTypes, accessToken}: {
     return records
 }
 
+const getRecordsByIds = async ({ids, accessToken}: {
+    ids: string[],
+    accessToken: string
+}): Promise<Item[]> => {
+    const DatoClient = new Client({apiToken: accessToken!})
+
+    const records = await DatoClient.items.list({
+        filter: {
+            ids: ids.join(), // The plugin ctx gives us an array of strings, but the CMA expects a CSV string
+        },
+    })
+    return records
+}
+
+const getValueByFieldPath = (formValues: Record<string, any>, fieldPath: string): any => {
+    return fieldPath.split('.').reduce((acc, key) => acc && acc[key], formValues);
+};
+
 export const SameLocaleLinks = ({ctx}: PropTypes) => {
-    const {locale, field, itemTypes, currentUserAccessToken, loadItemTypeFields} = ctx;
+    const {
+        locale,
+        field,
+        itemTypes,
+        currentUserAccessToken,
+        loadItemTypeFields,
+        setFieldValue,
+        fieldPath,
+        formValues
+    } = ctx;
 
     // @ts-expect-error We don't have strong typing for item type validations yet
-    const acceptableItemTypes: string[] = field.attributes.validators['items_item_type']['item_types'] // Array of item type IDs
+    const linkableItemTypeIds: string[] = field.attributes.validators['items_item_type']['item_types'] // Array of item type IDs
 
     const [linkableRecords, setLinkableRecords] = useState<Item[]>([])
-    const [titleFieldsByItemType, setTitleFieldsByItemType] = useState<{ [key: string]: string }>({})
-    const [localizedFieldsByItemType, setLocalizedFieldsByItemType] = useState<{ [key: string]: string[] }>({})
-    const [selectedRecords, setSelectedRecords] = useState<{value: string, label:string}[]>([]);
+    const [titleFieldsByItemTypeId, setTitleFieldsByItemTypeId] = useState<{ [key: string]: string }>({})
+    const [localizedFieldsByItemTypeId, setLocalizedFieldsByItemTypeId] = useState<{ [key: string]: string[] }>({})
+    const [selectedRecords, setSelectedRecords] = useState<{ value: string, label: string }[] | null>(null);
+
+    useEffect(() => {
+        (async () => {
+            for (const itemTypeId of linkableItemTypeIds) {
+                const fieldsForThisItemType: Field[] =  await loadItemTypeFields(itemTypeId)
+                const titleFieldId: string | undefined = itemTypes?.[itemTypeId]?.relationships?.title_field?.data?.id ?? undefined
+                const titleField = fieldsForThisItemType.find(field => field.id === titleFieldId)
+                if (titleField && titleField.attributes.api_key) {
+                    setTitleFieldsByItemTypeId(prevState => ({
+                        ...prevState,
+                        [itemTypeId]: titleField.attributes.api_key
+                    }))
+                }
+
+                const localizedFields = fieldsForThisItemType.filter(field => field.attributes.localized).map(field => field.attributes.api_key)
+                setLocalizedFieldsByItemTypeId(prevState => ({...prevState, [itemTypeId]: localizedFields}))
+            }
+        })()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [itemTypes, linkableItemTypeIds, locale])
+
+    const linkedRecordIds = useMemo<string[]>(() => getValueByFieldPath(formValues, fieldPath), [formValues, fieldPath])
+
+    useEffect(() => {
+        (async () => {
+            const records = await getRecordsByIds({ids: linkedRecordIds, accessToken: currentUserAccessToken!})
+
+            setSelectedRecords(records.map(record => {
+                const titleField = titleFieldsByItemTypeId[record.item_type.id]
+
+                // @ts-expect-error We don't know the title field's type
+                const recordName = record[titleField]?.[locale]
+
+                return {
+                    value: record.id,
+                    label: recordName ?? record.id
+                }
+            }))
+        })()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [linkedRecordIds, locale, titleFieldsByItemTypeId])
 
     useEffect(() => {
         if (!currentUserAccessToken) {
@@ -39,52 +107,52 @@ export const SameLocaleLinks = ({ctx}: PropTypes) => {
         }
 
         (async () => {
-            for (const itemTypeId of acceptableItemTypes) {
-                const titleFieldId: string | undefined = itemTypes?.[itemTypeId]?.relationships?.title_field?.data?.id ?? undefined
-                const fieldsForThisItemType = await loadItemTypeFields(itemTypeId)
-
-                const titleField = fieldsForThisItemType.find(field => field.id === titleFieldId)
-                if (titleField && titleField.attributes.api_key) {
-                    setTitleFieldsByItemType(prevState => ({...prevState, [itemTypeId]: titleField.attributes.api_key}))
-                }
-
-                const localizedFields = fieldsForThisItemType.filter(field => field.attributes.localized).map(field => field.attributes.api_key)
-                setLocalizedFieldsByItemType(prevState => ({...prevState, [itemTypeId]: localizedFields}))
-            }
 
             const recordsFromCMA = await getRecordsByItemTypes({
-                itemTypes: acceptableItemTypes,
+                itemTypes: linkableItemTypeIds,
                 accessToken: currentUserAccessToken! // TODO require this permission
             });
 
             setLinkableRecords(recordsFromCMA)
         })()
 
-    }, [currentUserAccessToken, itemTypes, locale, acceptableItemTypes, loadItemTypeFields])
+    }, [currentUserAccessToken, itemTypes, locale, linkableItemTypeIds])
 
-    const linkableRecordsWithAnyFieldInThisLocale = useMemo(() => {
-        // @ts-expect-error We don't know the field type, we just care about whether it has a localized key
-        const filteredRecords = linkableRecords.filter(record => localizedFieldsByItemType[record.item_type.id].some(field => record[field][locale]))
+    const linkableRecordsWithAnyFieldInThisLocale = useMemo<Item[]>(() => {
+        const filteredRecords = linkableRecords.filter(record => {
+            const fields = localizedFieldsByItemTypeId[record.item_type.id]
+            if (!fields?.length) return false
+            // @ts-expect-error We don't know what the record fields are, but that's OK as long as they have a non-empty locale
+            return fields.some(field => record[field][locale])
+        })
         return filteredRecords
-    }, [localizedFieldsByItemType, linkableRecords, locale])
+    }, [localizedFieldsByItemTypeId, linkableRecords, locale])
+
+    useEffect(() => {
+        if (selectedRecords === null) return
+        if (selectedRecords === getValueByFieldPath(formValues, fieldPath)) return
+        const selectedRecordIds = selectedRecords.map(record => record.value)
+        setFieldValue(fieldPath, selectedRecordIds)
+    }, [selectedRecords, setFieldValue, fieldPath, formValues])
 
     return (
         <Canvas ctx={ctx}>
             <Form>
                 <FieldGroup>
                     <SelectField
-                        name="multipleOption"
-                        id="multipleOption"
-                        label="Multiple options"
-                        hint="Select one of the options"
+                        name={`same-locale-links-${fieldPath}`}
+                        id={`same-locale-links-${fieldPath}`}
+                        label={`Only showing records in locale "${locale}"`}
+                        hint={`${selectedRecords?.length ?? 0} records selected`}
+                        placeholder={selectedRecords ? 'Click to select' : 'Loading, please wait...'}
                         value={selectedRecords}
                         selectInputProps={{
                             isMulti: true,
                             options: linkableRecordsWithAnyFieldInThisLocale.map(item => {
                                 const {id, item_type} = item;
                                 const itemTypeId = item_type.id
-                                const titleFieldName = titleFieldsByItemType[itemTypeId]
-                                // @ts-expect-error Our "record" types aren't set up right between the CMA and plugins SDK :(
+                                const titleFieldName = titleFieldsByItemTypeId[itemTypeId]
+                                // @ts-expect-error
                                 if (titleFieldName && item[titleFieldName][locale]) {
                                     // @ts-expect-error
                                     return {value: id, label: item[titleFieldName][locale] as string}
